a85338745799048c243076506dd095c976db4489
artem <artem@deffun.io>
2025-04-27 21:01:32 +0700

    add pyodide for local exec

--- src/coder/public/elements/MyMonaco.jsx
+++ src/coder/public/elements/MyMonaco.jsx
@@ -4,6 +4,7 @@ import { callFnState } from "@chainlit/react-client";
 
 export default function MonacoEditorComponent() {
   const editorRef = useRef(null);
+  const pyRef = useRef(null);
   const editorInstanceRef = useRef(null);
   const callFn = useRecoilValue(callFnState);
 
@@ -24,12 +25,11 @@ export default function MonacoEditorComponent() {
     }
 
     // If the script is not added yet, dynamically add it
-    const script = document.createElement("script");
-    script.src = "https://unpkg.com/monaco-editor@0.52.2/min/vs/loader.js";
-    script.async = true;
-    script.defer = true;
-    script.onload = initializeMonaco;
+    const script = newScript("https://unpkg.com/monaco-editor@0.52.2/min/vs/loader.js", initializeMonaco);
     document.head.appendChild(script);
+    // const pyScript = newScript("https://pyscript.net/releases/2025.3.1/core.js", null);
+    const pyScript = newScript("https://cdn.jsdelivr.net/pyodide/v0.27.5/full/pyodide.js", initializePyodide);
+    document.head.appendChild(pyScript);
 
     function initializeMonaco() {
       // Configure the path to Monaco modules via CDN
@@ -38,6 +38,26 @@ export default function MonacoEditorComponent() {
       });
       // Load the main editor module and initialize it
       window.require(["vs/editor/editor.main"], initializeEditor);
+    }
+    async function initializePyodide() {
+      let pyodide = await loadPyodide();
+      pyRef.current = pyodide;
+      // Pyodide is now ready to use...
+      console.log(pyRef.current.runPython(`
+        import sys
+        sys.version
+      `));
+    }
+
+    function newScript(src, onLoad) {
+      const script = document.createElement("script");
+      script.src = src;
+      script.async = true;
+      script.defer = true;
+      if (onLoad) {
+        script.onload = onLoad;
+      }
+      return script;
     }
   }, []);
 
@@ -67,7 +87,10 @@ export default function MonacoEditorComponent() {
 
   return (
     <div className="h-full w-full relative">
-      <div ref={editorRef} className="h-full w-full" />
+      <py-script>
+        import datetime as dt
+pyscript.write('today', dt.date.today().strftime('%A %B %d, %Y'))
+      </py-script>
     </div>
   );
 }
